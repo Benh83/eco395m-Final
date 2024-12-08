710e564f2dc9c99dfc2cde0da3e5cdb2b950d426
Vighnesh Avadhanam <vighnesh.avadhanam@utexas.edu>
2024-12-08 15:25:21 -0600

    Convering JSON output from perplexity to pandas dataframe and cleaning

---
+++ data_cleaning.py
@@ -0,0 +1,65 @@
+#JSON to pandas dataframe
+
+import json
+import pandas as pd
+import os
+import re
+
+
+pd.set_option('display.max_rows', None)  # No limit on rows
+pd.set_option('display.max_columns', None)  # No limit on columns
+
+BASE_DIR = "C:/Users/Admin/Desktop/Python-Class/eco395m-Final/data"
+JSONL_PATH = os.path.join(BASE_DIR, "results.jsonl")
+
+with open(JSONL_PATH, 'r') as file:
+    data = json.load(file)
+
+df = pd.json_normalize(data, sep='_')
+
+
+def url_links(dataframe):
+    """Takes a dataframe and returns a list of unique URL's in it"""
+    list_of_links = []
+    for column_name, column_data in dataframe.items():
+        if 'URL' in column_name or 'url' in column_name:
+            for value in column_data:
+                if value and value not in list_of_links:
+                    list_of_links.append(value)
+    return list_of_links
+
+
+def clean_text(dataframe):
+    """Cleans a the column names of the dataframe"""
+    dataframe.columns = [column_name.split("_")[0] for column_name in dataframe.columns]
+    dataframe = dataframe.loc[:, ~dataframe.columns.duplicated()]
+    
+    return dataframe
+
+
+def write_to_csv(dataframe):
+    links = url_links(dataframe) 
+    df_clean_text = clean_text(dataframe)
+    df_cleaned = df_clean_text.drop(columns=[column_name for column_name in df_clean_text.columns if 'URL' in column_name or 'url' in column_name])
+    df_cleaned["Links"] = [", ".join(links)] * len(df_cleaned)
+    df_cleaned.to_csv('m&a_data.csv', index=False)
+
+
+write_to_csv(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
